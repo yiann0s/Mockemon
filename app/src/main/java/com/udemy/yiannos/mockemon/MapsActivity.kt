2d4985ed9bb9514a612ea092b44d1aa2fe67fdc7
yiann0s <sobek1991@hotmail.com>
2018-12-12 16:21:57 +0200

    add mario icon and zoom

--- app/src/main/java/com/udemy/yiannos/mockemon/MapsActivity.kt
+++ app/src/main/java/com/udemy/yiannos/mockemon/MapsActivity.kt
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -37,7 +38,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.addMarker(MarkerOptions()
+            .position(sydney)
+            .title("Me")
+            .snippet(" here is my position")
+            .icon(BitmapDescriptorFactory.fromResource(R.drawable.mario)))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,14f))
     }
 }
