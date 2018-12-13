33bd156a09bfd1c5fe40faf9e517635025599b5f
yiann0s <sobek1991@hotmail.com>
2018-12-13 16:44:40 +0200

    create Mockemon class

--- app/src/main/java/com/udemy/yiannos/mockemon/Mockemon.kt
+++ app/src/main/java/com/udemy/yiannos/mockemon/Mockemon.kt
@@ -1,2 +1,12 @@
 package com.udemy.yiannos.mockemon
 
+class Mockemon{
+    var name:String?=null
+    var description:String?=null
+    var image:Int?=null
+    var power:Double?=null
+    var lat:Double?=null
+    var lng:Double?=null
+    constructor()
+
+}
